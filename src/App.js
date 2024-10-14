3628ede00c3aec3267068281d70179f9fa5811a1
hyojin kim <h20163820@gmail.com>
2024-10-15 06:26:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TodoLists from "./Components/TodoLists";
+import LoginPage from "./Components/LoginPage";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import RegisterPage from "./Components/RegisterPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/todo"
+          element={
+            <ProtectedRoute>
+              <TodoLists />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
   );
 }
 
